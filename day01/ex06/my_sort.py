0a05e67dcdd3fc5110d2e7f5cf7c25c651eafab2
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-11-09 00:37:10 +0100

    django piscine: ex06 in day 01 not completed

---
+++ day01/ex06/my_sort.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def print_sorted_dict(dic: dict) -> None:
+    print(dict(sorted(dic.items(), key=lambda item: item[1], reverse=False)))
+
+if __name__ == "__main__":
+    d = {
+        'Hendrix' : '1942',
+        'Allman' : '1946',
+        'King' : '1925',
+        'Clapton' : '1945',
+        'Johnson' : '1911',
+        'Berry' : '1926',
+        'Vaughan' : '1954',
+        'Cooder' : '1947',
+        'Page' : '1944',
+        'Richards' : '1943',
+        'Hammett' : '1962',
+        'Cobain' : '1967',
+        'Garcia' : '1942',
+        'Beck' : '1944',
+        'Santana' : '1947',
+        'Ramone' : '1948',
+        'White' : '1975',
+        'Frusciante': '1970',
+        'Thompson' : '1949',
+        'Burton' : '1939',
+    }
+print_sorted_dict(d)
+    
